3877ffcad2079f52084405b1aeaaf0e63952b834
Mohammed Asslahi <asslahi37@gmail.com>
2019-06-19 09:23:29 +0300

    leap year warmup

---
+++ Answercode.js
@@ -0,0 +1,13 @@
+const isleapYear = function(year){
+	if (year % 4===0 && year %!!100===0) { 
+	return true; }
+
+	if (year % 4===0 && year %100===0 && year %400===0){
+	return true;
+	}
+	
+
+     else {
+		return false;
+    }
+}
